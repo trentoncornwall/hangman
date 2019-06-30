7ba98f9750b49659d84c75522b95877fa9f70bc7
Trenton Cornwall <trenton434@gmail.com>
2019-06-30 15:51:57 -0600

    target word displays with under scores, incorrect letters display and add up properly, correct letters replace underscore however does not work words like programming when have multiple same letters, need additional logic

--- javascript.js
+++ javascript.js
@@ -1,33 +1,72 @@
-
 // initializing some variables
 
 
 // Word Library Object with functions that will control the game
 var wordLibrary = {
-    library: ["trenton","cornwall","best","computer","programming","code"],
-    
-    targetWord: function() {
+    library: ["trenton", "cornwall", "best", "computer", "programming", "code"],
+
+    targetWord: function () {
         var randWordNum = Math.floor(Math.random() * wordLibrary.library.length)
         return wordLibrary.library[randWordNum]
     }
 }
 
 
+
 // this will store the word the user needs to guess
 var targetWord = "";
 targetWord += wordLibrary.targetWord();
 
-// this will store keys they pressed
-var keysPressed = [];
+
+// this will store keys they pressed and fuction for DOM control
+var wrongKeysPressed = {
+    library: [],
+
+    print: function () {
+        document.getElementById("letterGuesses").textContent = this.library;
+    }
+};
+
+
+// this will story the target word 
+var correctKeysPressed = {
+    library: [],
+
+    print: function (x) {
+        document.getElementById("targetWord").textContent = this.library;
+    }
+}
+
+for (i = 0; i < targetWord.length; i++) {
+    correctKeysPressed.library.push("_")
+}
 
 
 
+correctKeysPressed.print()
 
 
 //USER FUNCTION
-document.onkeyup = function(event){
-
+document.onkeyup = function (event) {
     //assigns user key to variable userGuess
-    // var userGuess = event.key;
-    
+    var userGuess = event.key;
+    var guessIndex = targetWord.indexOf(userGuess);
+
+    if (guessIndex === -1) {
+
+        // adds to incorrect guesses
+        wrongKeysPressed.library.push(userGuess);
+
+        // calls fuction to display on screen
+        wrongKeysPressed.print()
+
+
+    } else {
+
+        correctKeysPressed.library[guessIndex] = userGuess;
+
+        correctKeysPressed.print()
+
+    }
+
 };
